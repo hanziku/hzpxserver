2f38343f3b90f78feec1da1ae482fe8048ddbb74
yapcheahshen <yapcheahshen@gmail.com>
2024-08-03 13:39:08 +0800

    accept color and size , return svg

--- server.js
+++ server.js
@@ -18,19 +18,29 @@ const fs=require('fs');
 const raw=fs.readFileSync(__dirname+'/glyphwiki-dump.txt');
 Hzpx.setFontTSV(raw);
 
+const resizeSVG=(svg,size=64)=>svg.replace(/(width|height)=\"\d+\"/g,(m,m1,m2)=>m1+'="'+size+'"');
+const patchColor=(svg,color)=>svg.replace(/fill="black"/g,'fill="'+color+'"');
+console.log('server started')
 //create a server object:
 http.createServer(function (req, res) {
   const query=url.parse(req.url,true).query;
   if (query.g) {
-    const data=Hzpx.drawPinx(query.g)[0];
+    let svg=Hzpx.drawPinx(query.g)[0];
     //5. Convert to jpeg file
-    svg2img(data, { format: 'jpg', 'quality': 75 }, function(error, buffer) {
-      //default jpeg quality is 75
-      res.writeHead(200, {"Content-Type": "image/jpg"});
-      res.write(buffer);
+    if (query.size) svg=resizeSVG( svg,parseInt(query.size)||24);
+    if (query.color) svg=patchColor( svg,query.color);
+    if (query.svg) {
+      res.writeHead(200, {"Content-Type": "image/svg","Access-Control-Allow-Origin":"*"});
+      res.write(svg);
       res.end();
-  })
-
+    } else {
+      svg2img(svg, { format: 'jpg', 'quality': 75 }, function(error, buffer) {
+        //default jpeg quality is 75
+        res.writeHead(200, {"Content-Type": "image/jpg","Access-Control-Allow-Origin":"*"});
+        res.write(buffer);
+        res.end();
+      })
+    }
   } else {
     res.writeHead(200, {"Content-Type": "text/plain"});
     res.write('error request'); //write a response to the client
